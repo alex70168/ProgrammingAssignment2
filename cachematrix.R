e6736f6abb22d08367f582ed3130fd8da17a1b9d
alex70168 <b00203012@gmail.com>
2016-04-06 12:06:16 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+##To solve the problem of large data, I wrote two functions to solve the inverse matrix problem .
 
-## Write a short comment describing this function
+##This function is meant to convert a matrix into a cached data. In 
+## doing so, one can reuse the data which is processed previously.
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ +inv <- NULL
+ +        set <- function(y) {
+ +                x <<- y
+ +                inv <<- NULL
+ +        }
+ +        get <- function() x
+ +        setinv <- function(x_inv)  inv <<- x_inv
+ +                getinv <- function() inv
+ +                list(set=set, get=get,setinv=setinv,getinv=getinv)
+  
+  }
+  
+  
+ +## cacheSolve function is meant to process the cached data.
+  
+  cacheSolve <- function(x, ...) {
+ -        ## Return a matrix that is the inverse of 'x'
+ +       inv<- x$getinv()
+ +        if(!is.null(inv)){
+ +                message("Getting cached data")
+ +                return(inv)
+ +        }
+ +        inv <- solve(x$get(),...)
+ +        x$setinv(inv)
+ +        inv
+  }
